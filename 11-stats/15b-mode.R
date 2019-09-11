141aca5745acccf3a60a49362e427d28a9fb2d43
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-09-11 21:08:23 +0530

    a

--- 11-stats/15b-mode.R
+++ 11-stats/15b-mode.R
@@ -2,6 +2,11 @@
 #https://cran.r-project.org/web/packages/modeest/modeest.pdf
 
 library(modeest)
+#
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("genefilter")
 
 mlv(mtcars$cyl,method='shorth')
 mlv(mtcars$cyl, method = "discrete")
---
+++ 93-IIMK/iimk1.R
@@ -0,0 +1,9 @@
+#IIMK
+
+#----- Completed
+#Data Structures
+
+
+
+
+#------- Continue
