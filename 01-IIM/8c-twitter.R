dc5203ab6b010058c104408ed7510f013ceb220f
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-11-23 13:09:13 +0530

    a

--- 01-IIM/8c-twitter.R
+++ 01-IIM/8c-twitter.R
@@ -23,7 +23,8 @@ twitter_token <- create_token(
   consumer_secret = consumerSecret,
   access_token = AccessToken,
   access_secret = AccessTokenSecret)
-
+#check token
+get_token()
 #search will Returns Twitter statuses matching a user provided search query. ONLY RETURNS DATA FROM THE PAST 6-9 DAYS. To return more than 18,000 statuses in a single call, set "retryonratelimit" to TRUE.
 
 searchString1 = '#sirmaur'   # change this to see others eg analytics...
@@ -99,4 +100,89 @@ ts_plot(ds, by = "days")
 #https://cran.r-project.org/web/packages/rtweet/rtweet.pdf
 #https://www.earthdatascience.org/courses/earth-analytics/get-data-using-apis/use-twitter-api-r/
 ?search_tweets  
-  
+#
+## search for 10,000 tweets sent from the US
+rt <- search_tweets(  "lang:en", geocode = lookup_coords("usa"), n = 10000)
+
+## create lat/lng variables using all available tweet and profile geo-location data
+rt <- lat_lng(rt)
+
+## plot state boundaries
+par(mar = c(0, 0, 0, 0))
+maps::map("state", lwd = .25)
+
+## plot lat and lng points onto state map
+with(rt, points(lng, lat, pch = 20, cex = .75, col = rgb(0, .3, .7, .75)))
+
+#----
+## random sample for 30 seconds (default)
+rt <- stream_tweets("",timeout=30)
+rt
+summary(rt)
+rt$text
+## stream tweets from london for 60 seconds
+rt <- stream_tweets(lookup_coords("london, uk"), timeout = 60)
+#needs api key... which may be on payment mode
+
+#----
+## stream london tweets for a week (60 secs x 60 mins * 24 hours *  7 days)
+stream_tweets(  "realdonaldtrump,trump",  timeout = 10,  file_name = "tweetsabouttrump.json",  parse = FALSE
+)
+
+## read in the data as a tidy tbl data frame
+djt <- parse_stream("tweetsabouttrump.json")
+djt
+djt$text
+## get user IDs of accounts followed by CNN
+cnn_fds <- get_friends("cnn")
+get_friends('dupadhyaya')
+## lookup data on those accounts
+cnn_fds_data <- lookup_users(cnn_fds$user_id)
+lookup_users(get_friends('dupadhyaya')$user_id)
+# get user IDs of accounts following CNN
+cnn_flw <- get_followers("cnn", n = 75)
+get_followers("dupadhyaya", n = 100)
+
+
+## lookup data on those accounts
+cnn_flw_data <- lookup_users(cnn_flw$user_id)
+lookup_users(get_friends('dupadhyaya')$user_id)
+## how many total follows does cnn have?
+cnn <- lookup_users("cnn")
+
+## get them all (this would take a little over 5 days)
+#cnn_flw <- get_followers(   "cnn", n = cnn$followers_count, retryonratelimit = TRUE)
+
+# get user IDs of accounts followed by CNN
+tmls <- get_timelines(c("cnn", "BBCWorld", "foxnews"), n = 10)
+
+## plot the frequency of tweets for each user over time
+tmls %>%  dplyr::filter(created_at > "2019-10-29") %>%
+  dplyr::group_by(screen_name) %>%
+  ts_plot("days", trim = 1L) +
+  ggplot2::geom_point() +
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    legend.title = ggplot2::element_blank(),
+    legend.position = "bottom",
+    plot.title = ggplot2::element_text(face = "bold")) +
+  ggplot2::labs(
+    x = NULL, y = NULL,
+    title = "Frequency of Twitter statuses posted by news organization",
+    subtitle = "Twitter status (tweet) counts aggregated by day from October/November 2017",
+    caption = "\nSource: Data collected from Twitter's REST API via rtweet"
+  )
+
+#Get the 30 most recently favorited statuses by JK Rowling.
+
+jkr <- get_favorites("jk_rowling", n = 30)
+jkr
+#Search for 10 users with the rstats hashtag in their profile bios.
+
+## search for users with #rstats in their profiles
+usrs <- search_users("#rstats", n = 100)
+usrs
+#Posting (tweeting from R console) or reading direct messages require additional permissions
+#If you’d like to post Twitter statuses, follow or unfollow accounts, and/or read your direct messages, you’ll need to create your own Twitter app
+#To create your own Twitter app, follow the instructions in the authorization vignette on obtaining and using access tokens
+#https://rtweet.info/articles/auth.html
