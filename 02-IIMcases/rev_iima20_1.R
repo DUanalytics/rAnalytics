2f5c7cc24090bdf0bc55d6506b9ae73f905ac710
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2020-09-07 18:57:25 +0530

    a

---
+++ 02-IIMcases/rev_iima20_1.R
@@ -0,0 +1,18 @@
+#Revision Session - IIMA - Sep 2020
+
+#vectors-----
+
+#matrices------
+
+#array--------
+
+#list---------
+
+
+#dataframe-----
+
+#factors-------
+
+
+#read/write - CSV, googlesheets
+
