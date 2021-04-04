31bbcfaccbfcd57ab2ab374fb866ce3ce74e841e
dupadhyaya <dup1966@gmail.com>
2021-04-04 09:48:09 +0530

    a

--- 08-MBB/03-MBB.R
+++ 08-MBB/03-MBB.R
@@ -9,15 +9,26 @@ names(mtcars) #names of variables/ columns
 #many other ways - nrow, dim, attributes
 
 #export as csv
-write.csv(mtcars, )
+write.csv(mtcars, 'mycars.csv', row.names = F)
+?write.csv  #help on command
+#a file with extension will be created which you can see under files tab (right bottom window of R Studio)
+dir()
+dir(pattern = '*.csv')
 
 # Importing through CSV-----
-
+newcars <- read.csv(file='mycars.csv')
+newcars  #see the data 
 
 #Importing through Excel ------
+install.packages('readxl')  #install this package
+#copy a simple excel file into the folder of project eg. myexcelcars.xlsx
+newexcelcars <- readxl::read_excel(path='myexcelcars.xlsx',sheet=1)
+newexcelcars
 
 #Importing through CSV URL ------
-
+url = 'https://raw.githubusercontent.com/DUanalytics/rAnalytics/master/data/denco.csv'
+dataURL <- read.csv(url)
+dataURL
 
 #Importing through Text File-----
 
