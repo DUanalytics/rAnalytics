fda7add25f916b29e568fbad9997218eb9c510d9
dupadhyaya <dup1966@gmail.com>
2022-08-16 18:54:57 +0530

    a

---
+++ 0-class/ximb_01.R
@@ -0,0 +1,22 @@
+#This is 2nd class of XIMB
+mtcars
+#control+enter
+x1 = c(1,4,6)
+x1[2]
+x1[c(1,3)]
+x2 <- c(4,35,34,77)
+x2
+print(x2)
+class(x1)
+#vector, matrics, dataframes - data structure
+mean(x1)
+(x3 = 1:100)  #assign and print
+min(x3)
+max(x3)
+sd(x3)
+x5 = c('A', "Chaman", "Divya")
+class(x5)
+x5
+x5[3]
+toupper(x5)
+
