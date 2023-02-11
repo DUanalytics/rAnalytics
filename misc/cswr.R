d2af38f056f148843d0ed1bb1b5cf8b6267ad015
dupadhyaya <dup1966@gmail.com>
2023-02-11 18:38:06 +0530

    a

---
+++ misc/cswr.R
@@ -0,0 +1,13 @@
+library(csvwr)
+
+d = data.frame(x=c('a','b','c'), y=1:3)
+d
+s = derive_table_schema(d)
+s
+
+m = create_metadata(tables=list(list(url='table.csv', tableSchema=s)))
+m
+
+j = jsonlite::toJSON(m)
+j
+cat(j, file='metadata.json')
